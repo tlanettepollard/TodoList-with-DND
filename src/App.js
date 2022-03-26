e6480f1dbb605c4be4c7ba95b7c37a36461c4dea
T. Lanette <TLPollard4516@gmail.com>
2022-03-26 12:34:06 -0400

    Added footer and form sections
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react'; 
 import Header from './components/Header';
+import Form from './components/Form';
+import Footer from './components/Footer';
 import ThemeProvider from './components/contexts/ThemeProvider';
+import { nanoid } from 'nanoid';
 import './scss/main.scss';
 
 
-function App() {
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+
+
+  function addTask(name) {
+    const newTask = { id: 'todo-' + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
 
   return (
     <ThemeProvider>
       <div className='wrapper'>
         <main className='main'>
           <Header />
+          <Form addTask={addTask}/>
         </main>
+        <Footer />
       </div>
     </ThemeProvider>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className='attribution'>
+            <p className='footer-text'>
+                Challenge by{''}
+                <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>
+            </p>
+            <p className='footer-text'>
+                Coded by <a href="https://tlp-portfolio.vercel.app/">Trista Lanette Pollard</a>.
+            </p>
+        </footer>
+    );
+}
