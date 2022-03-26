9c372301521ecec22dd974cc62409305f5542dc6
T. Lanette <TLPollard4516@gmail.com>
2022-03-25 22:41:15 -0400

    Added theme to app
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Header from './components/Header';
+import ThemeProvider from './components/contexts/ThemeProvider';
 import './scss/main.scss';
 
 
 function App() {
 
   return (
-    
-    <div className='wrapper'>
-      <main className='main'>
-        <Header />
-      </main>
-    </div>
-    
+    <ThemeProvider>
+      <div className='wrapper'>
+        <main className='main'>
+          <Header />
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import ThemeSetter from './ThemeSetter';
 
 const Header = () => {
     
     return (
         <header className='header-container'>
             <h1 className='title'>Todo</h1>
+            <ThemeSetter />
         </header>
     )
 }
